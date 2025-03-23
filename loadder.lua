5c93e3c2ab84745c19e3cb297788148201d8a20e
Zelda <166204162+Zeldazsy@users.noreply.github.com>
2025-03-23 21:24:58 +0700

    Create loadder.lua

---
+++ loadder.lua
@@ -0,0 +1,5 @@
+if game.GameId == 994732206
+loadstring(game:HtppGet("https://shorturl.at/U6mAa"))()
+elseif game.GameId == 5750914919 then
+loadstring(game:HttpGet("https://shorturl.at/Tfa1j"))()
+end
